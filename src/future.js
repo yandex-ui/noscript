a8c629da492314ab377aadbc8f2d60f21cc7ea27
Sergei Nikitin <nik.pasaran@gmail.com>
2011-10-05 15:54:48 +0400

    Futures допилены

--- src/future.js
+++ src/future.js
@@ -1,3 +1,7 @@
+// ------------------------------------------------------------------------------------------------------------- //
+// no.Future
+// ------------------------------------------------------------------------------------------------------------- //
+
 no.Future = function(worker) {
     this.worker = worker;
 };
@@ -10,29 +14,51 @@ no.Future.prototype.run = function(params) {
     return promise;
 };
 
-no.Future.wait = function(futures) {
-    var future = new no.Future();
+// ------------------------------------------------------------------------------------------------------------- //
 
-    future.run = function(promise, params) {
-        var promises = [];
-        for (var i = 0, l = futures.length; i < l; i++) {
-            promises.push( futures[i].run(params) );
-        }
-        return no.Promise.wait( promises );
-    };
-
-    return future;
+no.Future.Wait = function(futures) {
+    this.futures = futures;
 };
 
-no.Future.seq = function(futures) {
-    var future = new no.Future();
+no.Future.Wait.prototype.run = function(params) {
+    var promises = [];
 
-    future.run = function(promise, params) {
-        for (var i = 0, l = futures.length; i < l; i++) {
-            var future = futures[i];
+    var futures = this.futures;
+    for (var i = 0, l = futures.length; i < l; i++) {
+        promises.push( futures[i].run(params) );
+    }
+
+    return no.Promise.wait( promises );
+};
+
+// ------------------------------------------------------------------------------------------------------------- //
+
+no.Future.Seq = function(futures) {
+    this.futures = futures;
+};
+
+no.Future.Seq.prototype.run = function(params) {
+    var promise = new no.Promise;
+
+    var futures = this.futures;
+    var l = futures.length;
+
+    var results = [];
+    (function run(i, params) {
+        if (i < l) {
+            futures[i].run(params)
+                .then(function(result) {
+                    results[i] = result;
+                    run(i + 1, result);
+                })
+                .else_(function(error) {
+                    promise.reject(error);
+                });
+        } else {
+            promise.resolve(results);
         }
-    };
+    })(0, params);
 
-    return future;
+    return promise;
 };
 

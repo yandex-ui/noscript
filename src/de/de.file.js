945f915d0f05b30da2c27c0473ce0e9ff5a0dc4d
Sergei Nikitin <nik.pasaran@gmail.com>
2011-10-06 16:08:38 +0400

    file.(un)watch

--- src/de/de.file.js
+++ src/de/de.file.js
@@ -7,11 +7,6 @@ de.file = {};
 // ----------------------------------------------------------------------------------------------------------------- //
 
 de.file._cache = {};
-de.file._watched = {};
-
-// FIXME: Сейчас файл кэшируется навечно, что неправильно.
-//        Нужно или кэшировать на некоторое время (5 минут, например),
-//        или же следить за изменениями файла.
 
 de.file.get = function(filename) {
     var promise = de.file._cache[filename];
@@ -21,33 +16,51 @@ de.file.get = function(filename) {
 
         node.fs.readFile(filename, function(error, content) {
             if (error) {
+                delete de.file._cache[filename]; // Если не удалось считать файл, в следующий раз нужно повторить попытку,
+                                                 // а не брать из кэша ошибку.
                 promise.reject({
                     'id': 'FILE_OPEN_ERROR',
                     'message': error.message
                 });
             } else {
+                de.file.watch(filename); // Содержимое файла закэшировано внутри promise'а. Следим, не изменился ли файл.
                 promise.resolve(content);
             }
+
         });
-
-        if (!de.file._watched[filename]) { // FIXME: Непонятно, как это будет жить, когда файлов будет много.
-            de.file._watched[filename] = true;
-
-            // При изменении файла, удаляем его из кэша.
-            node.fs.watchFile(filename, function (/** @type {{mtime: Date}} */ curr, /** @type {{mtime: Date}} */ prev) {
-                if (curr.mtime !== prev.mtime) {
-                    no.events.trigger('file-changed', filename);
-                }
-            });
-        }
     }
 
     return promise;
 };
 
-no.events.bind('file-changed', function(e, filename) {
+no.events.bind('file-changed', function(e, filename) { // Файл изменился, выкидываем его из кэша.
     /** @type {string} */ filename;
 
     delete de.file._cache[ filename ];
+
+    // FIXME: Не нужно ли тут делать еще и unwatch?
 });
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+de.file._watched = {};
+
+de.file.watch = function(filename) {
+    if (!de.file._watched[ filename ]) { // FIXME: Непонятно, как это будет жить, когда файлов будет много.
+        de.file._watched[ filename ] = true;
+
+        node.fs.watchFile(filename, function (/** @type {{mtime: Date}} */ curr, /** @type {{mtime: Date}} */ prev) {
+            if (prev.mtime !== curr.mtime) {
+                no.events.trigger('file-changed', filename);
+            }
+        });
+    }
+};
+
+de.file.unwatch = function(filename) {
+    if (de.file._watched[ filename ]) {
+        delete de.file._watched[ filename ];
+        node.fs.unwatchFile(filename);
+    }
+};
 

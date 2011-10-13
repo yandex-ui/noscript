2da0a25156792dd0315d7905a3ce089cb8139492
chestozo <chestozo@yandex-team.ru>
2011-10-13 11:27:17 +0400

    Тесты.
    Добавил локально файлы для библиотек для тестирования: jquery, qunit,
    sinon.
    Создал файл тестов для no.Request.

--- tests/new/request.js
+++ tests/new/request.js
@@ -1,3 +1,15 @@
+include('../../src/no/no.js');
+include('../../src/no/no.promise.js');
+include('../../src/no/no.js');
+include('../../src/no/no.object.js');
+include('../../src/no/no.http.js');
+include('../../src/no/no.promise.js');
+include('../../src/no/no.model.js');
+
+include('../../src/no/no.request.js');
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
 module("Basic functionality");
 
 test("Creation", function() {

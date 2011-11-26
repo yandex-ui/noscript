dace97cb522375c15fb115f64fc1c46caf54a994
Sergei Nikitin <nik.pasaran@gmail.com>
2011-11-26 19:51:07 +0400

    Компиляция и get/set по а-ля jpath

--- src/no/no.path.js
+++ src/no/no.path.js
@@ -1,27 +1,69 @@
-no.path = function(s, dontCache) {
-    var path = no.path._cache[s];
+no.path = function(data, path, value) {
+    var path = new no.Path(path);
+    if (value) {
+        path.set(data, value);
+    } else {
+        return path.get(data);
+    }
+};
 
-    if (!path) {
-        path = no.path.compile(s);
-        if (!dontCache) {
-            no.path._cache[s] = path;
+no.Path = function(path) {
+    this.path = path;
+};
+
+no.Path._cache = {};
+
+no.Path.compile = function(path) {
+    var compiled = no.Path._cache[path];
+
+    if (!compiled) {
+        var parts = path.split('.');
+
+        var body = '';
+        for (var i = 0, l = parts.length; i < l; i++) {
+            var part = parts[i];
+            var r = part.match( /([\w-]+)(\[\d+\])?/ );
+            var step = r[1];
+            var index = r[2];
+
+            body += 'data = data["' + step + '"]; if (!data) { return; }';
+            if (index) {
+                body += 'data = data' + index + '; if (!data) { return; }';
+            }
         }
+        body += 'return data;';
+
+        compiled = no.Path._cache[path] = new Function('data', body);
     }
 
-    return path;
-}
-
-no.path._cache = {};
-
-no.path.get = function(data, path) {
-    no.todo();
+    return compiled;
 };
 
-no.path.set = function(data, path, value) {
-    no.todo();
+no.Path.prototype.get = function(obj) {
+    var getter = this.getter;
+    if (!getter) {
+        getter = no.Path.compile( this.path );
+    }
+    return getter(obj);
 };
 
-no.path.compile = function(path) {
-    no.todo();
+no.Path.prototype.set = function(obj, value) {
+    var setter = this.setter;
+    if (!setter) {
+        var path = this.path;
+        var r = /^(.*)(?:\[(\d+)\]|\.([\w-]+))$/.exec(path);
+        if (r === null) {
+            setter = function(obj, value) {
+                obj[path] = value;
+            };
+        } else {
+            var path = new no.Path(r[1]);
+            var index = r[2] || r[3];
+            setter = function(obj, value) {
+                path.get(obj)[index] = value;
+            }
+        }
+    }
+    setter(obj, value);
 };
 

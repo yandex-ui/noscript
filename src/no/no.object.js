018b30fd995ea9d7766b712f64e5a42007e49b2c
Sergei Nikitin <nik.pasaran@gmail.com>
2011-11-27 21:32:44 +0400

    object.isEqual

--- src/no/no.object.js
+++ src/no/no.object.js
@@ -20,18 +20,6 @@ no.object.keys = function(obj) {
 
 /**
     @param {!Object} obj
-    @return {boolean} Определяет, пустой ли объект или нет.
-*/
-no.object.isEmpty = function(obj) {
-    for (var key in obj) {
-        return false;
-    }
-
-    return true;
-};
-
-/**
-    @param {!Object} obj
     @return {Array.<string>} Возвращает список всех значений объекта.
 */
 no.object.values = function(obj) {
@@ -42,5 +30,45 @@ no.object.values = function(obj) {
     }
 
     return values;
+};
+
+/**
+    @param {!Object} obj
+    @return {boolean} Определяет, пустой ли объект или нет.
+*/
+no.object.isEmpty = function(obj) {
+    for (var key in obj) {
+        return false;
+    }
+
+    return true;
+};
+
+no.object.isEqual = function(a, b) {
+    if (a === b) { return true; }
+    if (a == null || b == null) { return false; }
+
+    var ta = typeof a;
+    var tb = typeof b;
+
+    if (ta !== tb) { return false; }
+
+    if (ta === 'object') {
+        for (var key in a) {
+            if (!no.object.isEqual(a[key], b[key])) { return false; }
+        }
+        // Проверяем, что в b нет "лишних" ключей (не входящих в a).
+        for (var key in b) {
+            if (!(key in a)) { return false; }
+        }
+    } else {
+        var l = a.length;
+        if (b.length !== l) { return false; }
+        for (var i = 0; i < l; i++) {
+            if (!no.object.isEqual(a[i], b[i])) { return false };
+        }
+    }
+
+    return true;
 };
 

934bec672b2b04320ae59e4fa1ff799389cef423
Sergei Nikitin <nik.pasaran@gmail.com>
2012-06-19 12:51:19 +0400

    Поправлен баг в isEqual

--- src/no/no.object.js
+++ src/no/no.object.js
@@ -44,9 +44,15 @@ no.object.isEmpty = function(obj) {
     return true;
 };
 
+//  FIXME: Как-то это все коряво. Нужно переписать и прооптимизировать.
 no.object.isEqual = function(a, b) {
     if (a === b) { return true; }
     if (a == null || b == null) { return false; }
+
+    var ta = typeof a;
+    var tb = typeof b;
+
+    if (ta !== tb) { return false; }
 
     //  NOTE: Удивительно, но все попытки сравнивать массивы не через for in
     //  приводят только к замедлению. По крайней мере, в FF.
@@ -58,12 +64,18 @@ no.object.isEqual = function(a, b) {
     //  оба цикла, а если объекты разные, то быстрее найти "лишний" ключ, чем
     //  сравнить все значения.
     //
-    for (var key in b) {
-        if ( !(key in a) ) { return false; }
-    }
+    if (typeof a === 'object') {
+        for (var key in b) {
+            if ( !(key in a) ) { return false; }
+        }
 
-    for (var key in a) {
-        if ( !no.object.isEqual( a[key], b[key] ) ) { return false; }
+        for (var key in a) {
+            if ( !no.object.isEqual( a[key], b[key] ) ) { return false; }
+        }
+    } else {
+        //  Это два скаляра (string, boolean, number) и они точно не равны,
+        //  т.к. это проверяется в первой строке метода.
+        return false;
     }
 
     return true;

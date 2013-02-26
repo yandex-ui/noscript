33337f3d5de54dbb24f3923fc2cacd7b4e97be70
Alexey Androsov <doochik@ya.ru>
2013-02-26 21:13:12 +0400

    заготовка для тестов

---
+++ test/spec/no.updater.js
@@ -0,0 +1,12 @@
+describe('no.Updater', function() {
+
+    describe('_getRequestViews', function() {
+        //sync, async, models
+    });
+
+    describe('_getUpdateTree', function() {
+        //sync, async, tree structure, add models to structure
+        //empty tree
+    });
+
+});

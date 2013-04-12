46da696a6561677e7d70be3d2f786517de3d801c
Alexey Androsov <doochik@ya.ru>
2013-04-12 13:58:20 +0400

    jsdoc

--- src/ns.log.js
+++ src/ns.log.js
@@ -1,6 +1,3 @@
-/**
- * @fileOverview
- */
 (function(/** @type ns */ns) {
 
     /**
@@ -9,15 +6,33 @@
      */
     ns.log = {};
 
-    ns.log.info = function(name, data) {
+    /**
+     * Логирует сообщение.
+     * @param {String} name Название сообщение.
+     * @param {Object} [data] Данные
+     * @param {String} [longText] Дополнительные данные. Длинная строка, которую надо разбить на части.
+     */
+    ns.log.info = function(name, data, longText) {
 
     };
 
-    ns.log.error = function(name, data) {
+    /**
+     * Логирует ошибку.
+     * @param {String} name Название ошибки.
+     * @param {Object} [data] Данные
+     * @param {String} [longText] Дополнительные данные. Длинная строка, которую надо разбить на части.
+     */
+    ns.log.error = function(name, data, longText) {
 
     };
 
-    ns.log.exception = function(name, exception) {
+    /**
+     * Логирует JS-исключение.
+     * @param {String} name Название ошибки.
+     * @param {Error} exception Пойманное исключение.
+     * @param {Object} [data] Дополнительные данные.
+     */
+    ns.log.exception = function(name, exception, data) {
 
     };
 

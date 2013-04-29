f9a3cfd95015bcbbe95f06bb06682259d44085b2
Alexey Androsov <aandrosov@yandex-team.ru>
2013-04-30 00:36:25 +0400

    grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+/*global module*/
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha');
+
+    var gruntConfig = {};
+    gruntConfig.jshint = {
+        options: {
+            jshintrc: '.jshintrc'
+        },
+        files: [
+            'src/*.js'
+        ]
+    };
+
+    gruntConfig.mocha = {
+        index: ['test/index.html']
+    };
+
+    grunt.initConfig(gruntConfig);
+
+    grunt.registerTask('default', ['mocha']);
+};

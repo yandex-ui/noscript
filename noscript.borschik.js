8829c0d9e84c0c1a85b823516c4e356233385f77
Alexey Androsov <doochik@ya.ru>
2013-08-26 17:43:48 +0400

    Add build files to npm

---
+++ noscript.borschik.js
@@ -0,0 +1,34 @@
+(function(window, document) {
+    "use strict";
+    /* borschik:include:node_modules/nommon/lib/no.base.js */
+    /* borschik:include:node_modules/nommon/lib/no.events.js */
+    /* borschik:include:node_modules/nommon/lib/no.promise.js */
+
+    /* borschik:include:node_modules/nommon/lib/no.parser.js */
+    /* borschik:include:node_modules/nommon/lib/no.jpath.js */
+
+    /* borschik:include:src/ns.js */
+    /* borschik:include:src/ns.consts.js */
+    /* borschik:include:src/ns.consts.events.js */
+    /* borschik:include:src/ns.dom.js */
+    /* borschik:include:src/ns.entityify.js */
+    /* borschik:include:src/ns.history.js */
+    /* borschik:include:src/ns.http.js */
+    /* borschik:include:src/ns.object.js */
+
+    /* borschik:include:src/ns.action.js */
+    /* borschik:include:src/ns.box.js */
+    /* borschik:include:src/ns.model.js */
+    /* borschik:include:src/ns.modelCollection.js */
+    /* borschik:include:src/ns.layout.js */
+    /* borschik:include:src/ns.log.js */
+    /* borschik:include:src/ns.page.js */
+    /* borschik:include:src/ns.request.js */
+    /* borschik:include:src/ns.router.js */
+    /* borschik:include:src/ns.update.js */
+    /* borschik:include:src/ns.view.js */
+    /* borschik:include:src/ns.viewCollection.js */
+
+    window.no = no;
+    window.ns = ns;
+})(window, document);

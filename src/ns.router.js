2acf30d02377557d359f939e1e1c0e1b5de4a568
chestozo <chestozo@yandex-team.ru>
2013-09-05 00:58:36 +0400

    ns.router.generateUrl метод для генерации урла роутером

--- src/ns.router.js
+++ src/ns.router.js
@@ -46,14 +46,15 @@ ns.router = function(url) {
 
         var r = route.regexp.exec(url);
         if (r) {
-            var tokens = route.tokens;
-            var defaults = route.defaults;
+            var rparams = route.params;
             var params = {};
 
-            // Вытаскиваем параметры из основной части урла. Имена параметров берем из массива tokens.
-            var l = tokens.length;
+            // Вытаскиваем параметры из основной части урла.
+            var l = rparams.length;
+            var rparam;
             for (var k = 0; k < l; k++) {
-                params[ tokens[k] ] = r[ k + 1 ] || defaults[ tokens[k] ];
+                rparam = rparams[k];
+                params[rparam.name] = r[k + 1] || rparam.default_value;
             }
 
             // Смотрим, есть ли дополнительные get-параметры, вида ?param1=value1&param2=value2...
@@ -88,7 +89,50 @@ ns.router = function(url) {
  * @return {String} Valid url that takes into consideration baseDir.
  */
 ns.router.url = function(url) {
-    return ns.router.baseDir + url;
+    return (ns.router.baseDir + url) || '/';
+};
+
+ns.router.generateUrl = function(id, params) {
+    params = params || {};
+
+    var result = [];
+    var _routeDef = ns.router._routes.routeHash[id];
+
+    if (!_routeDef) {
+        throw new Error("[ns.router] Could not find route with id '" + id  +"'!");
+    }
+
+    var _rpart;
+    var pvalue;
+    for (var i = 0; i < _routeDef.parts.length; i++) {
+        _rpart = _routeDef.parts[i];
+        if (!_rpart.name) {
+            result.push(_rpart.default_value);
+        } else {
+            pvalue = params[_rpart.name] || _rpart.default_value;
+
+            // Обязательный параметр должен быть указан.
+            if (!_rpart.is_optional && !pvalue) {
+                throw new Error("[ns.router] Parameter '" + _rpart.name + "' must be specified");
+            }
+
+            // Опциональный параметр не должен попасть в урл, если он не указан явно в params.
+            if (_rpart.is_optional && !(_rpart.name in params)) {
+                continue;
+            }
+
+            // Проверка типа.
+            if (!ns.router._regexps[_rpart.type].test(pvalue)) {
+                throw new Error("[ns.router] Parameter '" + _rpart.name + "' type must be '" + _rpart.type + "'");
+            }
+
+            result.push(pvalue);
+        }
+    }
+
+    var _url = result.join('/');
+    _url = _url ? ('/' + _url) : '';
+    return ns.router.url(_url);
 };
 
 /**
@@ -105,14 +149,23 @@ ns.router.init = function() {
     var rawRoutes = routes.route || {};
 
     var compiledRoutes = [];
+    var compiledRoutesHash = {};
     for (var route in rawRoutes) {
         var compiled = ns.router.compile(route);
         compiled.page = rawRoutes[route];
         compiledRoutes.push(compiled);
+        compiledRoutesHash[ rawRoutes[route] ] = compiled;
     }
     _routes.route = compiledRoutes;
+    _routes.routeHash = compiledRoutesHash;
 
     ns.router._routes = _routes;
+
+    // Типы нужны при генерации урла.
+    ns.router._regexps = {};
+    for (var id in ns.router.regexps) {
+        ns.router._regexps[id] = new RegExp( ns.router.regexps[id] );
+    }
 };
 
 /**
@@ -121,55 +174,90 @@ ns.router.init = function() {
  * @return {{ regexp: RegExp, tokens: Array.<string> }}
 */
 ns.router.compile = function(route) {
-    //  Отрезаем последний слэш, он ниже добавится как необязательный.
-    var regexp = route.replace(/\/$/, '');
+    //  /a/b/ -> a/b
+    route = route
+        .replace(/^\//, '')
+        .replace(/\/$/, '');
 
-    var tokens = [];
-    var defaults = {};
+    var parts = route.split('/');
+    var params = parts.map(ns.router.__parseParam);
+    var pregexps = params.map(ns.router.__generateParamRegexp);
+    var regexp = pregexps.join('');
 
-    //  Заменяем {name} на кусок регэкспа соответствующего типу токена name.
-    //  Матч на слеш нужен, чтобы сделать слеш опциональным.
-    regexp = regexp.replace(/(\/?){(.*?)}/g, function(_, slash, token) {
-        var tokenParts = token.split(':');
-        slash = slash || '';
-
-        var type = tokenParts[1] || 'id';
-        var rx_part = ns.router.regexps[type];
-        if (!rx_part) {
-            throw new Error("[ns.router] Can't find regexp for '" + type +"'!");
-        }
-
-        var tokenName = tokenParts[0];
-        var equalSignIndex = tokenName.indexOf('=');
-
-        if (equalSignIndex > 0) {
-            var tokenDefault = tokenName.substring(equalSignIndex + 1);
-            tokenName = tokenName.substring(0, equalSignIndex);
-
-            tokens.push(tokenName);
-            defaults[tokenName] = tokenDefault;
-            if (slash) {
-                return '(?:' + slash + '(' + rx_part + '))?';
-            }
-            else {
-                return '(' + rx_part + ')?';
-            }
-
-        } else {
-            //  Запоминаем имя токена, оно нужно при парсинге урлов.
-            tokens.push(tokenName);
-            return slash + '(' + rx_part + ')';
-        }
-    });
     //  Добавляем "якоря" ^ и $;
     //  Плюс матчим необязательный query-string в конце урла, вида ?param1=value1&param2=value2...
     regexp = '^' + regexp + '\/?(?:\\?(.*))?$';
 
     return {
         regexp: new RegExp(regexp),
-        tokens: tokens,
-        defaults: defaults
+        params: params.filter(function(p) { return !!p.name; }), // оставляем только настоящие параметры, а не статические части урла
+        parts: params // для генерации урла нужны все параметры
     };
+};
+
+ns.router.__parseParam = function(param) {
+    var param_extract;
+    var type_parts;
+    var default_parts;
+    var param_type;
+    var param_default;
+    var param_is_optional;
+
+    // it can be a parameter (in curly braces)
+    // the most complex case: name=default:type
+    param_extract = /{([^}]+)}/.exec(param);
+    if (param_extract) {
+        param = param_extract[1];
+
+        // parameter type (defaults to id)
+        type_parts = param.split(':');
+        param_type = type_parts[1] || 'id';
+
+        // parameter default value and if parameter is optional
+        param = type_parts[0];
+        default_parts = param.split('=');
+        param_default = default_parts[1];
+        param_is_optional = (default_parts.length > 1);
+
+        // section parsed
+        param = default_parts[0];
+        return {
+            name: param,
+            type: param_type,
+            default_value: param_default,
+            is_optional: param_is_optional
+        };
+    }
+    // it can be a static text
+    else {
+        return {
+            default_value: param
+        };
+    }
+};
+
+ns.router.__generateParamRegexp = function(p) {
+    var re;
+    var regexps = ns.router.regexps;
+
+    // static text
+    if (p.default_value && !p.name) {
+        return '/' + p.default_value;
+    }
+
+    // validate parameter type is known (if specified)
+    if (p.type && !(p.type in regexps)) {
+        throw new Error("[ns.router] Could not find regexp for '" + p.type +"'!");
+    }
+
+    re = regexps[p.type];
+    re = '/(' + re + ')';
+
+    if (p.is_optional) {
+        re = '(?:' + re + ')?';
+    }
+
+    return re;
 };
 
 /**

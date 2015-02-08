118a13a6cefcc7075d857bb718c97884109f4be2
Aleksei Androsov <doochik@ya.ru>
2015-02-08 14:21:19 +0300

    Определить возможные вариантаы объявления лайаутов #510 - remove params
    Больше не надо передавать params в beforeUpdateHtml

--- src/ns.update.js
+++ src/ns.update.js
@@ -329,7 +329,7 @@
             'ns-view-hide': [],
             'ns-view-htmldestroy': []
         };
-        this.view.beforeUpdateHTML(this.params, hideViewEvents);
+        this.view.beforeUpdateHTML(hideViewEvents);
         this._triggerViewEvents(hideViewEvents);
 
         var viewEvents = {

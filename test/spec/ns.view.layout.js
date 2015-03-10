654f4c6e2706c5d106140398c49696c225f4e8aa
Aleksei Androsov <doochik@ya.ru>
2015-03-10 22:59:23 +0300

    [ns.View] Через patchLayout нельзя менять детей. Fix #533

--- test/spec/ns.view.layout.js
+++ test/spec/ns.view.layout.js
@@ -1,4 +1,4 @@
-describe('ns.View dynamic layouts ->', function() {
+describe.only('ns.View dynamic layouts ->', function() {
 
     describe('Простой случай ->', function() {
 
@@ -10,37 +10,29 @@ describe('ns.View dynamic layouts ->', function() {
                 }
             });
 
-            ns.layout.define('view2-layout', {
-                'view3': {}
-            });
-
-            this.view2ModelStateFn = this.sinon.stub();
-            ns.Model.define('view2-state', {
-                methods: {
-                    isOk: this.view2ModelStateFn
+            ns.layout.define('view-layout', {
+                'box@': {
+                    'view3': {}
                 }
             });
+
+            ns.layout.define('view-empty', {
+                'box@': {}
+            });
+
+            this.view2GetLayoutFn = this.sinon.stub();
 
             ns.View.define('view1');
             ns.View.define('view2', {
-                models: {
-                    'view2-state': true
-                },
                 methods: {
                     patchLayout: function() {
-                        if (this.getModel('view2-state').isOk()) {
-                            return 'view2-layout';
-                        }
-                    }
-                }
-            });
+                        return this.getLayout();
+                    },
 
-            this.view3DestroyFn = this.sinon.stub();
-            ns.View.define('view3', {
-                methods: {
-                    destroy: this.view3DestroyFn
+                    getLayout: this.view2GetLayoutFn
                 }
             });
+            ns.View.define('view3');
 
             this.view = ns.View.create('view1');
 
@@ -50,44 +42,43 @@ describe('ns.View dynamic layouts ->', function() {
         describe('Отрисовка ->', function() {
 
             it('Должен создать view3, если отдали такой layout', function() {
-                this.view2ModelStateFn.returns(true);
+                this.view2GetLayoutFn.returns('view-layout');
 
                 return new ns.Update(this.view, ns.layout.page('app'), {})
                     .render()
                     .then(function() {
-                        expect(this.view.node.querySelectorAll('.ns-view-view2>.ns-view-view3')).to.have.length(1);
+                        expect(this.view.node.querySelectorAll('.ns-view-view2 .ns-view-view3')).to.have.length(1);
                     }, null, this);
             });
 
             it('Не должен создать view3, если не отдали layout', function() {
-                this.view2ModelStateFn.returns(false);
+                this.view2GetLayoutFn.returns('view-empty');
 
                 return new ns.Update(this.view, ns.layout.page('app'), {})
                     .render()
                     .then(function() {
-                        expect(this.view.node.querySelectorAll('.ns-view-view2>.ns-view-view3')).to.have.length(0);
+                        expect(this.view.node.querySelectorAll('.ns-view-view2 .ns-view-view3')).to.have.length(0);
                     }, null, this);
             });
 
-        });
+            it('Должен правильно перерисовывать виды при изменении layout', function() {
+                this.view2GetLayoutFn.returns('view-empty');
 
-        describe('Удаление вложенных видов ->', function() {
-
-            beforeEach(function() {
-                this.view2ModelStateFn.returns(true);
-
-                // сначала рисуем сначал раскладку, в которой есть view3
                 return new ns.Update(this.view, ns.layout.page('app'), {})
                     .render()
                     .then(function() {
-                        // потом рисуем раскладку, в которой нет view3
-                        this.view2ModelStateFn.returns(false);
-                        return new ns.Update(this.view, ns.layout.page('app'), {}).render();
-                    }, null, this);
-            });
+                        this.view2GetLayoutFn.returns('view-layout');
+                        return this.view.update();
+                    }, null, this)
+                    .then(function() {
+                        expect(this.view.node.querySelectorAll('.ns-view-view2 .ns-view-view3.ns-view-visible')).to.have.length(1);
 
-            it('должен вызвать #destroy у удаленного вида', function() {
-                expect(this.view3DestroyFn).to.have.callCount(1);
+                        this.view2GetLayoutFn.returns('view-empty');
+                        return this.view.update();
+                    }, null, this)
+                    .then(function() {
+                        expect(this.view.node.querySelectorAll('.ns-view-view2 .ns-view-view3.ns-view-hidden')).to.have.length(1);
+                    }, null, this);
             });
 
         });

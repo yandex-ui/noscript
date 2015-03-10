1322b592ebc37350a335f0cbe2d5ab19c826c0c5
Aleksei Androsov <doochik@ya.ru>
2015-03-10 23:51:29 +0300

    Дать возможность менять корневой узел для ns.View
    Сделал тесты лучше

--- test/spec/yate.view.js
+++ test/spec/yate.view.js
@@ -30,29 +30,20 @@ describe('рендеринг ns.View через yate', function() {
         expect(this.view.node.innerHTML).to.contain('<div>test-view-matcher key view=view-test-matcher</div>');
     });
 
-    it('должен переопределить корневой тэг для вьюхи span-tag-view', function(done) {
+    it('должен переопределить корневой тэг для вьюхи span-tag-view', function() {
         ns.View.define('span-tag-view');
         ns.layout.define('span-test', {
             'span-tag-view': {}
         });
 
-        vSpanTagView = ns.View.create('span-tag-view');
+        var vSpanTagView = ns.View.create('span-tag-view');
         var layout = ns.layout.page('span-test', {});
 
-        new ns.Update(vSpanTagView, layout, {})
+        return new ns.Update(vSpanTagView, layout, {})
             .render()
-            .then(
-                function() {
-                    if (vSpanTagView.node.tagName === 'SPAN') {
-                        done();
-                    } else {
-                        done('Tag name expected to be SPAN');
-                    }
-                },
-                function(e) {
-                    done(e);
-                }
-            );
+            .then(function() {
+                expect(vSpanTagView.node.tagName).to.be.equal('SPAN');
+            });
     });
 
 });
